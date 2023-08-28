908368582725e2980b88be160c28fba6587ced33
Clayton Zambon <zambon@outlook.com.br>
2023-08-28 15:05:45 -0300

    FEAT: Criação do projeto do Jogo da Adivinhação em C plus plus

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main(){
+	
+	setlocale(LC_ALL, "");
+	cout << "*************************************" << endl;
+	cout << "* Bem Vindo ao Jogo da Adivinhação! *" << endl;
+	cout << "*************************************" << endl;
+	
+	return 0;
+}
