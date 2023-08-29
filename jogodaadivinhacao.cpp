82c575032cdbd9481bdc801bcd94025950c192b2
Clayton Zambon <zambon@outlook.com.br>
2023-08-28 22:10:28 -0300

    FEAT: Armazenando o numero secreto

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -8,5 +8,7 @@ int main(){
 	cout << "* Bem Vindo ao Jogo da Adivinhacao! *" << endl;
 	cout << "*************************************" << endl;
 	
-	return 0;
+	int numero_secreto = 42;
+	
+	cout << "O numero secreto eh " << numero_secreto << ". Nao conte para ninguem." << endl;
 }
