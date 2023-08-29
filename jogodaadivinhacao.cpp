7b613d893c5f72daa3b95114b7665a85b94511b1
Clayton Zambon <zambon@outlook.com.br>
2023-08-28 22:18:32 -0300

    FEAT: Chutando numeros

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -10,5 +10,18 @@ int main(){
 	
 	int numero_secreto = 42;
 	
-	cout << "O numero secreto eh " << numero_secreto << ". Nao conte para ninguem." << endl;
+	int chute;
+	cout << endl << "Qual seu chute? ";
+	cin >> chute;
+	cout << "O valor do seu chute eh: " << chute << endl;
+	
+	if (chute == numero_secreto){
+		cout << "Parabens! Voce acertou o numero secreto!" << endl;
+	}
+	else if (chute > numero_secreto){
+		cout << "Seu chute foi maior que o numero secreto." << endl;
+	}
+	else {
+		cout << "Seu chute foi menor que o numero secreto." << endl;
+	}
 }
