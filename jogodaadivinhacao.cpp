adbf0b67b6c39a7fee9bc54715f6e165a77f896b
Clayton Zambon <zambon@outlook.com.br>
2023-08-28 22:01:43 -0300

    FEAT: Primeiro Ajuste e mensagem de boas vindas

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -1,13 +1,11 @@
 ﻿#include <iostream>
-#include <locale.h>
 
 using namespace std;
 
 int main(){
 	
-	setlocale(LC_ALL, "");
 	cout << "*************************************" << endl;
-	cout << "* Bem Vindo ao Jogo da Adivinhação! *" << endl;
+	cout << "* Bem Vindo ao Jogo da Adivinhacao! *" << endl;
 	cout << "*************************************" << endl;
 	
 	return 0;
