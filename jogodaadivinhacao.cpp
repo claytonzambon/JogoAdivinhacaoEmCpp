974d8595b4fe34e6ab4e186c1321520836f42744
Clayton Zambon <zambon@outlook.com.br>
2023-08-28 22:24:20 -0300

    FEAT: Novos tipos em Cpp

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -8,20 +8,24 @@ int main(){
 	cout << "* Bem Vindo ao Jogo da Adivinhacao! *" << endl;
 	cout << "*************************************" << endl;
 	
-	int numero_secreto = 42;
+	const int NUMERO_SECRETO = 42;
 	
 	int chute;
 	cout << endl << "Qual seu chute? ";
 	cin >> chute;
 	cout << "O valor do seu chute eh: " << chute << endl;
 	
-	if (chute == numero_secreto){
+	bool acertou = chute == NUMERO_SECRETO;
+	bool maior = chute > NUMERO_SECRETO;
+	bool menor = chute < NUMERO_SECRETO;
+	
+	if (acertou){
 		cout << "Parabens! Voce acertou o numero secreto!" << endl;
 	}
-	else if (chute > numero_secreto){
+	else if (maior){
 		cout << "Seu chute foi maior que o numero secreto." << endl;
 	}
-	else {
+	else if (menor){
 		cout << "Seu chute foi menor que o numero secreto." << endl;
 	}
 }
